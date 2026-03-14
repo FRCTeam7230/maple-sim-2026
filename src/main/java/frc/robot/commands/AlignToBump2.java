f3a6e32f34bf3e1282406bd6d95ceda92bb654c3
arkamandalnandi-cyber <arkamandalnandi@gmail.com>
2026-03-14 12:15:42 -0700

    Added Comments on AlignToBump2
    Added Comments on AlignToBump2

--- src/main/java/frc/robot/commands/AlignToBump2.java
+++ src/main/java/frc/robot/commands/AlignToBump2.java
@@ -8,6 +8,7 @@ import frc.robot.Constants;
 import frc.robot.subsystems.drive.Drive;
 
 public class AlignToBump2 extends Command{//This is the better edition
+    /** PID Controller for rotation of robot(goal orientation is diagonal) */
     PIDController rotController = new PIDController(0.04, 0, 0.001);
     Drive m_drive;
     double currentAngle;
@@ -42,6 +43,7 @@ public class AlignToBump2 extends Command{//This is the better edition
         // currentAngle += 360;
         // }
 
+        //This makes sure the robot angle between 0 to 360, so we can use it to determine the current robot heading
         drivingOverTheBumpDirectionMode = 0;
         currentAngle = m_drive.getPose().getRotation().getDegrees();
         while (currentAngle > 360 || currentAngle<0) {
@@ -73,6 +75,8 @@ public class AlignToBump2 extends Command{//This is the better edition
         // } else {
         //     rotController.setSetpoint(angle1);
         // }
+
+        //used to find closest angle and sets as target angle
         if (currentAngle>90&&currentAngle<180){
             rotController.setSetpoint(angle2);
         } else if (currentAngle>180&&currentAngle<270){
@@ -91,6 +95,8 @@ public class AlignToBump2 extends Command{//This is the better edition
     @Override
     public void execute() {
        // SmartDashboard.putNumber("AlignToBump/driveState", drivingOverTheBumpDirectionMode);
+
+       //Drives to position
         switch (drivingOverTheBumpDirectionMode){
             case 1:
             case 3:
@@ -115,11 +121,13 @@ public class AlignToBump2 extends Command{//This is the better edition
         }
     }
     @Override
+    //stops the robot at the end of the command
     public void end(boolean interrupted) {
         m_drive.stop();
     }
     @Override 
     public boolean isFinished(){
+        //checks if it is finished by checking if robot has driven over bump
         switch (drivingOverTheBumpDirectionMode){
             case 1:
                 return m_drive.getPose().getX()>4.626+0.5588+odomError;
@@ -133,6 +141,7 @@ public class AlignToBump2 extends Command{//This is the better edition
                 return false;
         }
     }
+    /** Returns the driving direction based on the robot's current x position */
     public int findDrivingDirection(){
         double xPos = m_drive.getPose().getX();//This cannnot be updated in periodic
         if (xPos<8.256) { //If the robot is on the blue side of the field
